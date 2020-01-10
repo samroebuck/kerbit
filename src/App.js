dede0d3b63dc58d61953fc162d59778a46f1e15d
Sam Roebuck <sam@sarrymac.local>
2020-01-10 17:29:39 +0000

    model added, camera control added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
 import React from "react";
 import "./App.css";
-// import Webcam from "react-webcam";
-import CameraControl from './CameraControl'
-
-
+import CameraControl from "./CameraControl";
+import { Webcam } from "./Webcam.js";
+import Model from './Model.js'
 
 class App extends React.Component {
-  render() {
-   
+  constructor(props) {
+    super(props);
+    this.webcam = null;
+    this.state = {
+      capturedImage: null,
+      captured: false
+    };
+  }
 
-    return <CameraControl />;
+  initializeCamera = () => {
+    // initialize the camera
+    this.canvasElement = document.createElement("canvas");
+    this.webcam = new Webcam(
+      document.getElementById("webcam"),
+      this.canvasElement
+    );
+    this.webcam.setup().catch(() => {
+      alert(
+        "Error getting access to your camera, open browser in a standalone window"
+      );
+    });
+  }
+
+  captureImage = async () => {
+    const capturedData = this.webcam.takeBase64Photo({
+      type: "jpeg",
+      quality: 0.8
+    });
+    // console.log(capturedData);
+    this.setState({
+      captured: true,
+      capturedImage: capturedData.base64
+    });
+  };
+
+  discardImage = () => {
+    this.setState({
+      captured: false,
+      capturedImage: null
+    });
+  };
+
+  render() {
+    return (
+      <>
+      <CameraControl
+        captureImage={this.captureImage}
+        discardImage={this.discardImage}
+        capturedImage={this.state.capturedImage}
+        initializeCamera={this.initializeCamera}
+      />
+      <Model {...this.state}/>
+      </>
+    );
   }
 }
 
