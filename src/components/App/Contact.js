d6eeed7f926bc681e7e474aed5f544c8c174befa
Sam Roebuck <samiiroebuck.96@gmail.com>
2020-04-30 17:05:20 +0100

    resetting form

--- src/components/App/Contact.js
+++ src/components/App/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from 'react';
 import {
   Link
 } from "react-router-dom";
@@ -6,49 +6,10 @@ import {
 import BackArrow from '../../images/backArrow.svg'
 import AppBar from './AppBar';
 
-const onSubmit = async (event, setSubmitText) => {
-  event.preventDefault();
-  setSubmitText("Submitting ...");
-  const formElements = [...event.currentTarget.elements];
-  const isValid =
-    formElements.filter(elem => elem.name === "bot-field")[0].value === "";
 
-  const validFormElements = isValid ? formElements : [];
+const Contact = props => {
 
-  if (validFormElements.length < 1) {
-    //or some other cheeky error message
-    setSubmitText("It looks like you filled out too many fields!");
-  } else {
-    const filledOutElements = validFormElements
-      .filter(elem => !!elem.value)
-      .map(
-        element =>
-          encodeURIComponent(element.name) +
-          "=" +
-          encodeURIComponent(element.value)
-      )
-      .join("&");
-
-    await fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: filledOutElements
-    })
-      .then(() => {
-        setSubmitText("Successfully submitted!");
-      })
-      .catch(_ => {
-        setSubmitText(
-          "There was an error with your submission, please email me using the address above."
-        );
-      });
-  }
-};
-
-
-const Contact = () => {
-  const [submitText, setSubmitText] = useState(null);
-   return (
+  return (
     <>
       <AppBar></AppBar>
       <Link to=''className='return'><img src={BackArrow} alt='back arrow'/></Link>
@@ -60,13 +21,7 @@ const Contact = () => {
             <span>LET US KNOW!</span>
             
           </h3>
-          <form name='contact' netlify='true' className='form' method='POST' onSubmit={e => onSubmit(e, setSubmitText)}>
-          <p style={{ display: "none" }}>
-          <label>
-            Don’t fill this out if you expect to hear from us!
-            <input name="bot-field" value="" readOnly />
-          </label>
-        </p>
+          <form name='contact' netlify='true' className='form' method='POST'>
             <input type='hidden' name='form-name' value='contact' />
 
             <label htmlFor='name' className='form__label'>
@@ -108,12 +63,9 @@ const Contact = () => {
           </form>
 
         </div>
-        <p>{submitText}</p>
       </section>
     </>
   );
-   }
-
-
+};
 
 export default Contact;
