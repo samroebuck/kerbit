76ada7a381971416e3db205260e712883f20a885
Sam Roebuck <samiiroebuck.96@gmail.com>
2020-04-30 16:16:31 +0100

    stupif form

--- src/components/App/Contact.js
+++ src/components/App/Contact.js
@@ -1,122 +1,84 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import {
+  Link
+} from "react-router-dom";
 // Images
-import BackArrow from '../../images/backArrow.svg';
+import BackArrow from '../../images/backArrow.svg'
 import AppBar from './AppBar';
 
-const encode = (data) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
-};
 
-class Contact extends React.Component {
+class Contact extends React.Component  {
   constructor(props) {
     super(props);
-    this.state = { name: '', email: '', message: '' };
+    this.state = { submitted: ''};
   }
 
-  /* Here’s the juicy bit for posting the form submission */
-
-  handleSubmit = (e) => {
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'kerbit-wrong', ...this.state }),
+  handleSubmit = e => {
+    this.setState({
+      submitted: 'Thanks for the submission'
     })
-      .then(() => alert('Success!'))
-      .catch((error) => alert(error));
-
-    e.preventDefault();
   };
 
-  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+render() {
+   return (
+    <>
+      <AppBar></AppBar>
+      <Link to=''className='return'><img src={BackArrow} alt='back arrow'/></Link>
+      <section className='contactpage'>
+        <div className='contactpage__container'>
+          <h3 className='contactpage__title'>
+            SOMETHING WRONG WITH KERBIT?
+            <br></br>
+            <span>LET US KNOW!</span>
+            
+          </h3>
+          <form name='kerbit-wrong' netlify='true' className='form' method='POST' action='/thank-you'>
+            <input type='hidden' name='form-name' value='kerbit-wrong' />
 
-  render() {
-    const { name, email, message } = this.state;
-    return (
-      <>
-        <AppBar></AppBar>
-        <Link to='' className='return'>
-          <img src={BackArrow} alt='back arrow' />
-        </Link>
-        <section className='contactpage'>
-          <div className='contactpage__container'>
-            <h3 className='contactpage__title'>
-              SOMETHING WRONG WITH KERBIT?
-              <br></br>
-              <span>LET US KNOW!</span>
-            </h3>
-            {/* <form onSubmit={this.handleSubmit}>
-              <input type='hidden' name='form-name' value='kerbit-wrong' />
+            <label htmlFor='name' className='form__label'>
+            Name
+            </label>
+            <input
+              className='form__wronginput'
+              type='text'
+              name='name'
+              required
+              placeholder='Name'
+            />
 
-              <label htmlFor='name' className='form__label'>
-                Name
-              </label>
-              <input
-                className='form__wronginput'
-                type='text'
-                name='name'
-                required
-                placeholder='Name'
-                value={name}
-                onChange={this.handleChange}
-              />
+            <label htmlFor='email' className='form__label'>
+              Email
+            </label>
+            <input
+              className='form__rightinput'
+              type='email'
+              name='email'
+              required
+              placeholder='Email'
 
-              <label htmlFor='email' className='form__label'>
-                Email
-              </label>
-              <input
-                className='form__rightinput'
-                type='email'
-                name='email'
-                required
-                placeholder='Email'
-                value={email}
-                onChange={this.handleChange}
-              />
+            />
 
-              <label htmlFor='message' className='form__extralabel'>
-                What can we do for you?
-              </label>
-              <textarea
-                className='form__extrainfo'
-                name='message'
-                rows='3'
-                placeholder="What's the problem?"
-                value={message}
-                onChange={this.handleChange}
-              ></textarea>
+            <label htmlFor='message' className='form__extralabel'>
+              What can we do for you?
+            </label>
+            <textarea
+              className='form__extrainfo'
+              name='message'
+              rows='3'
+              placeholder="What's the problem?"
+            ></textarea>
 
-              <button className='form__btn' type='submit'>
-                Send
-              </button>
-            </form> */}
-            <form onSubmit={this.handleSubmit}>
-              <p>
-                <label>
-                  Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Message: <textarea name="message" value={message} onChange={this.handleChange} />
-                </label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
-            </form>
-          </div>
-        </section>
-      </>
-    );
-  }
-}
+            <button className='form__btn' type='submit'>
+              Send
+            </button>
+          </form>
 
-export default withRouter(Contact);
+        </div>
+        <p>{this.state.submitted}</p>
+      </section>
+    </>
+  );
+   }
+};
+
+export default Contact;
