50e940dd02e2c5c56bb4570b5f0f2bf21bf7c0bf
Sam Roebuck <samiiroebuck.96@gmail.com>
2020-04-30 15:26:42 +0100

    more damn form stuff

--- src/components/App/Contact.js
+++ src/components/App/Contact.js
@@ -1,31 +1,76 @@
-import React from 'react';
-import {
-  Link
-} from "react-router-dom";
+import React, {useState}  from 'react';
+import { Link } from 'react-router-dom';
 // Images
-import BackArrow from '../../images/backArrow.svg'
+import BackArrow from '../../images/backArrow.svg';
 import AppBar from './AppBar';
 
+const Contact = () => {
 
-const Contact = props => {
+  const [name,setName] = useState('');
+  const [status,setStatus] = useState('');
+  const [email,setEmail] = useState('');
+  const [message,setMessage] = useState('');
 
+
+  const encode = (data) => {
+    const formData = new FormData();
+    Object.keys(data).forEach((k)=>{
+      formData.append(k,data[k])
+    });
+    return formData
+  }
+
+  const handleSubmit = e => {
+    const data = { "form-name": "contact", name, email, message}
+    
+    fetch("/", {
+      method: "POST",
+      // headers: { "Content-Type": 'multipart/form-data; boundary=random' },
+      body: encode(data)
+    })
+      .then(() => setStatus("Form Submission Successful!!"))
+      .catch(error => setStatus("Form Submission Failed!"));
+
+    e.preventDefault();
+  };
+
+  const handleChange = e => {
+    const {name, value} = e.target
+    if (name === 'name' ){
+      return setName(value)
+    }
+    if (name === 'email' ){
+      return setEmail(value)
+    }
+    if (name === 'message' ){
+      return setMessage(value)
+    }
+  }
   return (
     <>
       <AppBar></AppBar>
-      <Link to='' className='return'><img src={BackArrow} alt='back arrow'/></Link>
+      <Link to='' className='return'>
+        <img src={BackArrow} alt='back arrow' />
+      </Link>
       <section className='contactpage'>
         <div className='contactpage__container'>
           <h3 className='contactpage__title'>
             SOMETHING WRONG WITH KERBIT?
             <br></br>
             <span>LET US KNOW!</span>
-            
           </h3>
-          <form name='kerbit-wrong' netlify='true' className='form' action='/FormSuccess' method='POST'>
+          <form
+            name='kerbit-wrong'
+            netlify='true'
+            className='form'
+            action='/FormSuccess'
+            method='POST'
+            onSubmit={handleSubmit}
+          >
             <input type='hidden' name='form-name' value='kerbit-wrong' />
 
             <label htmlFor='name' className='form__label'>
-            Name
+              Name
             </label>
             <input
               className='form__wronginput'
@@ -33,6 +78,7 @@ const Contact = props => {
               name='name'
               required
               placeholder='Name'
+              value={name} onChange={handleChange} 
             />
 
             <label htmlFor='email' className='form__label'>
@@ -44,7 +90,7 @@ const Contact = props => {
               name='email'
               required
               placeholder='Email'
-
+              value={email} onChange={handleChange} 
             />
 
             <label htmlFor='message' className='form__extralabel'>
@@ -55,16 +101,17 @@ const Contact = props => {
               name='message'
               rows='3'
               placeholder="What's the problem?"
+              value={message} onChange={handleChange} 
             ></textarea>
 
             <button className='form__btn' type='submit'>
               Send
             </button>
           </form>
+          <h3>{status}</h3>
         </div>
       </section>
     </>
   );
 };
-
 export default Contact;
